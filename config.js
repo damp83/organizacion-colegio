dbaddbd40be6189bf70121324e705d9c35ce090d
damp83 <diegoalberto.moya@gmail.com>
2025-09-04 00:00:39 +0200

    subitr

---
+++ config.js
@@ -0,0 +1,16 @@
+// Configuración de la app (puedes cambiar el app_id si quieres segmentar datos por app)
+window.__app_id = 'organizacioncentro-d3cd7';
+
+// Configuración de Firebase (objeto). Puedes dejar measurementId aunque no usemos Analytics.
+window.__firebase_config = {
+  apiKey: "AIzaSyDVBTBgqKP_4uf8GHh4F5lhvcnOedXjR_Y",
+  authDomain: "organizacioncentro-d3cd7.firebaseapp.com",
+  projectId: "organizacioncentro-d3cd7",
+  storageBucket: "organizacioncentro-d3cd7.firebasestorage.app",
+  messagingSenderId: "1018056761030",
+  appId: "1:1018056761030:web:9912a01455832ac2bb81c7",
+  measurementId: "G-ZV16BRW2W2"
+};
+
+// Si usas autenticación con token personalizado, descomenta y completa:
+// window.__initial_auth_token = "TOKEN_JWT";
