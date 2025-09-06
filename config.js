f19748d9349a13f0d29d6078564c03a311c9bb05
damp83 <diegoalberto.moya@gmail.com>
2025-09-06 12:13:24 +0200

    a

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ window.__firebase_config = {
   apiKey: "AIzaSyDVBTBgqKP_4uf8GHh4F5lhvcnOedXjR_Y",
   authDomain: "organizacioncentro-d3cd7.firebaseapp.com",
   projectId: "organizacioncentro-d3cd7",
-  storageBucket: "organizacioncentro-d3cd7.firebasestorage.app",
+  // storageBucket omitido porque no usamos Firebase Storage (archivos en Firestore base64)
   messagingSenderId: "1018056761030",
   appId: "1:1018056761030:web:169222b13f897228bb81c7",
   measurementId: "G-NQJQW02JDY"
